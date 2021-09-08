92893294817c4365f4f4bbeeec3f5b679d0719a6
DJ Drakos <dianajodrakos@gmail.com>
2021-09-08 09:45:57 -0700

    created display components

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Display from '../random-color/Display';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <>
+    <Display />
+  </>;
 }
---
+++ src/components/random-color/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = () => {
+  return <p>Display</p>;
+};
+
+Display.PropTypes = {
+
+};
+
+export default Display;
