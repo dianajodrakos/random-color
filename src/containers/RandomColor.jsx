b897e093a2fe21be686a93fbcb4ecc25e4aa3d07
DJ Drakos <dianajodrakos@gmail.com>
2021-09-20 09:33:01 -0700

    add color change function

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import Display from '../components/random-color/Display';
 import { randomColor } from 'randomcolor';
@@ -6,6 +7,19 @@ export default class RandomColor extends Component {
 
 state = {
   color: randomColor(),
+}
+
+handleColorChange = () => {
+  const newColor = randomColor();
+  if(newColor === this.state.color) {
+    this.setState({ color: 'url(https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg)' });
+  } else {
+    this.setState({ color: newColor });
+  }
+}
+
+componentDidMount() {
+  setInterval(() => this.handleColorChange(), 1000);
 }
 
 render() {
